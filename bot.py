99d92d546b87174279735204f2b60344f61d207e
L1MJH <lolflgc@icloud.com>
2023-09-03 20:56:14 +0800

    Added leetcode tracking

--- bot.py
+++ bot.py
@@ -20,7 +20,39 @@ def load_members():
     except FileNotFoundError:
         return set()
 
+def save_data():
+    with open("daily_progress.txt", "w") as dp_file:
+        for user, progress in daily_progress.items():
+            dp_file.write(f"{user},{progress}\n")
+
+    with open("penalties.txt", "w") as pen_file:
+        for user, penalty in penalties.items():
+            pen_file.write(f"{user},{penalty}\n")
+
+def load_data():
+    daily_progress = {}
+    penalties = {}
+
+    try:
+        with open("daily_progress.txt", "r") as dp_file:
+            for line in dp_file:
+                user, progress = line.strip().split(',')
+                daily_progress[user] = progress == 'True'
+    except FileNotFoundError:
+        pass
+
+    try:
+        with open("penalties.txt", "r") as pen_file:
+            for line in pen_file:
+                user, penalty = line.strip().split(',')
+                penalties[user] = int(penalty)
+    except FileNotFoundError:
+        pass
+
+    return daily_progress, penalties
+
 group_members = load_members()
+daily_progress, penalties = load_data()
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
@@ -37,6 +69,7 @@ def send_help(message):
     /start, /hello - Start the bot and add your username to the group members.
     /add [username] - Add a specified username to the group members list.
     /members - Show the list of all group members.
+    /daily - Declare your daily LeetCode completion.
     /help - Display this help text.
     """
     bot.send_message(message.chat.id, help_text)
@@ -51,6 +84,16 @@ def add_member(message):
     except IndexError:
         bot.reply_to(message, "Please provide a member name after the /add command.")
     
+@bot.message_handler(commands=['daily'])
+def daily_declaration(message):
+    user_name = message.from_user.username
+    if user_name:
+        daily_progress[user_name] = True  # Mark the user's progress for the day as complete
+        save_data()
+        bot.reply_to(message, "Your LeetCode completion for today has been recorded. Well done!")
+    else:
+        bot.reply_to(message, "Error: Couldn't retrieve your username. Please ensure you have a username set on Telegram.")
+
 @bot.message_handler(commands=['members'])
 def show_members(message):
     if group_members:
@@ -62,8 +105,13 @@ def show_members(message):
 def check_time():
     while True:
         now = datetime.now()
-        if now.hour == 17 and now.minute == 35:  # Adjust the time as needed
-            bot.send_message(-801071288, "It's 5am! Time for something special!")
+        if now.hour == 5 and now.minute == 0:
+            for user in group_members:
+                if not daily_progress.get(user, False):  # If the user hasn't marked their progress
+                    penalties[user] = penalties.get(user, 0) + 10  # Add $10 penalty
+            daily_progress.clear()  # Reset daily progress for the next day
+            save_data()
+            bot.send_message(-801071288, "Checked daily LeetCode progress and penalties have been updated!")
             time.sleep(60)  # Sleep for 60 seconds to avoid multiple notifications
         else:
             time.sleep(10)  # Sleep for 10 seconds before checking again
