24c2ff1fe882f34f2222ea97636882e2e997b5e6
L1MJH <lolflgc@icloud.com>
2023-09-03 20:33:07 +0800

    Add add member function

--- bot.py
+++ bot.py
@@ -8,19 +8,32 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 bot.send_message(-801071288, "I'm alive!")
 
+group_members = set()
+
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
+    user_name = message.from_user.username
+    if user_name:
+        group_members.add(user_name)
     bot.reply_to(message, "Howdy, how are you doing?")
 
-@bot.message_handler(func=lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=['add'])
+def add_member(message):
+    try:
+        member_name = message.text.split()[1]  # Extract the member name after the '/add' command
+        group_members.add(member_name)
+        bot.reply_to(message, f"Added {member_name} to the group members list.")
+    except IndexError:
+        bot.reply_to(message, "Please provide a member name after the /add command.")
+    
+@bot.message_handler(commands=['members'])
+def show_members(group_members):
+    bot.send_message(-801071288, group_members)
 
 def check_time():
     while True:
         now = datetime.now()
-        if now.hour == 17 and now.minute == 35:
-            # Do something specific at 5am
+        if now.hour == 17 and now.minute == 35:  # Adjust the time as needed
             bot.send_message(-801071288, "It's 5am! Time for something special!")
             time.sleep(60)  # Sleep for 60 seconds to avoid multiple notifications
         else:
