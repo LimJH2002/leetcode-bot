0d1c5453e8496955f3014924d817d4c3e27c96a3
L1MJH <lolflgc@icloud.com>
2023-09-03 20:41:26 +0800

    Add read and write files

--- bot.py
+++ bot.py
@@ -8,13 +8,26 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 bot.send_message(-801071288, "I'm alive!")
 
-group_members = set()
+def save_members():
+    with open("members.txt", "w") as f:
+        for member in group_members:
+            f.write(f"{member}\n")
+
+def load_members():
+    try:
+        with open("members.txt", "r") as f:
+            return set(line.strip() for line in f)
+    except FileNotFoundError:
+        return set()
+
+group_members = load_members()
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
     user_name = message.from_user.username
     if user_name:
         group_members.add(user_name)
+        save_members()
     bot.reply_to(message, "Howdy, how are you doing?")
 
 @bot.message_handler(commands=['add'])
@@ -22,6 +35,7 @@ def add_member(message):
     try:
         member_name = message.text.split()[1]  # Extract the member name after the '/add' command
         group_members.add(member_name)
+        save_members()  # Save the updated list
         bot.reply_to(message, f"Added {member_name} to the group members list.")
     except IndexError:
         bot.reply_to(message, "Please provide a member name after the /add command.")
