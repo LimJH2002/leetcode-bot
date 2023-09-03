5b55d54bbbefbb7feadd3f9bfbc02636c6aaf223
L1MJH <lolflgc@icloud.com>
2023-09-03 17:23:34 +0800

    Update auto check time

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import os
+import telebot
+import threading
+import time
 from datetime import datetime
 
-
-import telebot
-
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
-
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
@@ -16,13 +15,17 @@ def send_welcome(message):
 def echo_all(message):
     bot.reply_to(message, message.text)
 
-def echo_all(message):
-    now = datetime.now()
-    if now.hour == 5 and now.minute == 0:
-        # Do something specific at 5am
-        bot.reply_to(message, "It's 5am! Time for something special!")
-    else:
-        bot.reply_to(message, message.text)
+def check_time():
+    while True:
+        now = datetime.now()
+        if now.hour == 5 and now.minute == 0:
+            # Do something specific at 5am
+            print("It's 5am! Time for something special!")
+            time.sleep(60)  # Sleep for 60 seconds to avoid multiple notifications
+        else:
+            time.sleep(10)  # Sleep for 10 seconds before checking again
 
+thread = threading.Thread(target=check_time)
+thread.start()
 
 bot.infinity_polling()
